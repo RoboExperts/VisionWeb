cc85d95348d01db2801eb202f95cc1f6b2dd1587
RoboExperts <shakimirza@gmail.com>
2018-09-03 12:23:12 +0500

    Darjleen Pizza Files Here

---
+++ js/scroller.js
@@ -0,0 +1,5 @@
+sts_bs("jwscroller2dbd",[20080623,"images/","","blank.gif",3,1,10,20,"1044px","left",3,0,324,203,0,0,0,0,0,5000,1,40,1,"stEffect(\"scroll(Rate=24,enabled=0,Duration=0.88)\")",-2,22],["none",1,"#454545","transparent","","no-repeat"]);
+sts_ai("i0",[0,"","","_self","add1.jpg",324,203,"center"],["transparent","9pt Verdana,Arial","#000000","none","9pt Verdana,Arial","#000000","none"]);
+sts_ai("i1",[,,,,"add2.jpg"],[],"i0","i0");
+sts_ai("i2",[,,,,"add3.jpg"],[],"i0","i0");
+sts_es();
